551c6a51bbb2d4a921e3518088046ef3b03cfa09
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-19 03:13:13 +0900

    지역 유효성 검증

---
+++ backend/app/Http/Requests/Region/StoreRegionRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Region;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRegionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'query' => ['sometimes', 'nullable', 'string'],
+            'country' => ['sometimes', 'nullable', 'string']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'query.string' => '쿼리의 값은 반드시 문자형이어야 합니다.',
+            'country.string'=> '국가코드의 값은 반드시 문자형이어야 합니다.'
+        ];
+    }
+}
