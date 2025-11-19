64cb39b1f12b83116fed89bd6ebedce688ec1b77
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-19 18:24:40 +0900

    아이템 추가 유효성검증

--- backend/app/Http/Requests/Trip/StoreItemRequest.php
+++ backend/app/Http/Requests/Trip/StoreItemRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Trip;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class StoreItemRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreItemRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::check();
     }
 
     /**
@@ -22,7 +24,20 @@ class StoreItemRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'place_id' => ['required', 'integer', 'min:1', Rule::exists('places', 'place_id')],
+            'seq_no' => ['required', 'integer', 'min:1'],
+            'visit_time'=> ['sometimes', 'nullable', 'date_format:Y-m-d H:i']
         ];
+    }
+    
+    public function messages(): array
+    {
+        return [
+            'place_id.required' => '장소 ID는 필수입니다.',
+            'place_id.integer'  => '장소 ID는 숫자여야 합니다.',
+            'place_id.exists'   => '존재하지 않는 장소입니다.',
+            'seq_no.integer'    => '순서는 숫자여야 합니다.',
+            'visit_time.date_format' => '방문 시간 형식이 올바르지 않습니다.'
+                ];
     }
 }
