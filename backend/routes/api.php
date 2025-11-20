bfb0be12088bac9042ca474901be3732e9c70355
Jung_Min <162419902+jammmin02@users.noreply.github.com>
2025-11-20 23:55:23 +0900

    Feature/repository trip (#12)
    * update(router) : trip, tripday, scheduleitem 라우터 작성
    
    * update(repository) : 공통 base repository 작성 (CRUD)
    
    * update(repository) : trip repository 작성
    
    * update(repository) :  create 메서드 명 수정

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Backend\App\Http\Controllers\AuthController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\TripsController;
+use App\Http\Controllers\TripDayController;
+use App\Http\Controllers\ScheduleItemController;
+use App\Http\Controllers\PlaceController;
+use App\Http\Controllers\RegionController;  
 
-Route::post('/api/v1/auth/signup', [AuthController::class, 'signup']);
+
+/**
+ * Trips API Routes
+ */
+Route::apiResource('trips', TripsController::class);
+
+/**
+ * Trip Days API Routes
+ */
+Route::apiResource('trip.days', TripDayController::class)->shallow();
+
+/**
+ * Schedule Items API Routes
+ */
+Route::apiResource('days.items', ScheduleItemController::class)->shallow();
