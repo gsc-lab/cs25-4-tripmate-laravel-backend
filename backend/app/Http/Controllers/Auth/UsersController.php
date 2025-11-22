b44519c61f7a1be0489c3fbfb2ded6330936465d
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-23 05:26:21 +0900

    usercontroller 룆ㄱ

---
+++ backend/app/Http/Controllers/Auth/UsersController.php
@@ -0,0 +1,45 @@
+<?php
+
+    namespace App\Http\Controllers\Auth;
+
+    use App\Http\Controllers\Controller;
+    use App\Services\Auth\UserService;
+    use App\Http\Requests\Auth\AuthVerificationRequest;
+    use Illuminate\Support\Facades\Auth;
+
+    class UsersController extends Controller
+    {
+        private UserService $userService;
+        public function __construct(UserService $userService)
+        {
+            $this->userService = $userService;
+        }
+
+        /**
+         * User Mypage
+         */
+        public function getCurrentUser() 
+        {
+            $user = $this->userService->currentUser();
+
+            return response()->json([
+                'success' => true,
+                'data'=> $user
+            ]);
+        }
+
+        /**
+         * User delete - 회원삭제
+         */
+        public function deleteCurrentUser(AuthVerificationRequest $request) 
+        {
+            $data = $request->validated();
+
+            $userId = Auth::id();
+
+            $this->userService->deleteUser($userId, $data["password"]);
+
+            return response()->noContent();
+        }
+
+    }
