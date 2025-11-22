f5a2429868d617ea609a18ea5839150ec56c4ca6
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-22 21:34:01 +0900

    메서드 반환값 타입힌팅

--- backend/app/Services/Auth/AuthService.php
+++ backend/app/Services/Auth/AuthService.php
@@ -33,7 +33,7 @@
          * User Login Service
          * @param array $data
          */
-        public function loginUser(string $email, string $password)
+        public function loginUser(string $email, string $password):mixed
         {
             // 유저 조회
             $user = $this->authRepository->findUserEmail($email);
