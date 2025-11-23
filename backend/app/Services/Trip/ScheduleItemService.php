544cc57dbc8102003127c72cdef6d140b6198829
jammmin02 <jmkm1201@naver.com>
2025-11-23 16:56:00 +0900

    update(service) : 수정 및 조회 삭제 메서드 추가

--- backend/app/Services/Trip/ScheduleItemService.php
+++ backend/app/Services/Trip/ScheduleItemService.php
@@ -26,7 +26,7 @@ class ScheduleItemService
             $this->tripDayRepository = $rsvpDayRepository;
         }
     
-   /**
+    /**
     * 내부 공통 헬퍼 메서드
     * - Trip + day_no로 Trip_day_id 조회
     * - 없으면 ModelNotFoundException 예외 발생
@@ -47,7 +47,7 @@ class ScheduleItemService
             );
 
             // 없으면 예외 발생
-            if (!$tripDayId) {
+            if (is_null($tripDayId)) {
                 throw new ModelNotFoundException("해당하는 Trip Day를 찾을 수 없습니다");
             }
 
@@ -101,71 +101,270 @@ class ScheduleItemService
         );
     }
 
-    // /**
-    //  * 3. ScheduleItem 생성
-    //  * - seq_no가 null이면 해당 TripDay의 마지막 seq_no + 1로 설정
-    //  * - seq_no가 있으면 해당 seq_no 이후의 항목들의 seq_no 1씩 증가
-    //  * @param Trip $trip
-    //  * @param int $dayNo
-    //  * @param int|null $seqNo
-    //  * @param int|null $placeId 
-    //  * @param string|null $visitTime
-    //  * @param string|null $memo
-    //  * @return ScheduleItem
-    //  * @throws ModelNotFoundException
-    //  */
-    // public function createScheduleItem(
-    //     Trip $trip, 
-    //     int $dayNo,
-    //     ?int $seqNo,
-    //     ?int $placeId,
-    //     ?string $visitTime,
-    //     ?string $memo
-    //     ): ScheduleItem {
-    //         // Trip_day_id 조회
-    //         $tripDayId = $this->getTripDayIdOrFail($trip, $dayNo);
+    /**
+     * 3. ScheduleItem 생성
+     * - seq_no가 null이면 해당 TripDay의 마지막 seq_no + 1로 설정
+     * - seq_no가 있으면 해당 seq_no 이후의 항목들의 seq_no 1씩 증가
+     * @param Trip $trip
+     * @param int $dayNo
+     * @param int|null $seqNo
+     * @param int|null $placeId 
+     * @param string|null $visitTime
+     * @param string|null $memo
+     * @return ScheduleItem
+     * @throws ModelNotFoundException
+     */
+    public function createScheduleItem(
+        Trip $trip, 
+        int $dayNo,
+        ?int $seqNo,
+        ?int $placeId,
+        ?string $visitTime,
+        ?string $memo
+        ): ScheduleItem {
+            // Trip_day_id 조회
+            $tripDayId = $this->getTripDayIdOrFail($trip, $dayNo);
 
-    //         return DB::transaction(function () use (
-    //             $tripDayId,
-    //             $seqNo,
-    //             $placeId,
-    //             $visitTime,
-    //             $memo
-    //         ) {
+            return DB::transaction(function () use (
+                $tripDayId,
+                $seqNo,
+                $placeId,
+                $visitTime,
+                $memo
+            ) {
 
-    //             // 현재 최대 seq_no 조회
-    //             $maxSeqNo = $this->scheduleItemRepository->getMaxSeqNo($tripDayId);
+                // 현재 최대 seq_no 조회
+                $maxSeqNo = $this->scheduleItemRepository->getMaxSeqNo($tripDayId);
 
-    //             // seq_no가 null이면 마지막 seq_no + 1로 설정
-    //             if (is_null($seqNo)) {
-    //                 $seqNo = $maxSeqNo + 1;
-    //             } else {
-    //                 // 최소값 보정 (1보다 작으면 1로 설정)
-    //                 if ($seqNo < 1) {
-    //                     $seqNo = 1;
-    //                 }
+                // seq_no가 null이면 마지막 seq_no + 1로 설정
+                if (is_null($seqNo)) {
+                    $seqNo = $maxSeqNo + 1;
+                } else {
+                    // 최소값 보정 (1보다 작으면 1로 설정)
+                    if ($seqNo < 1) {
+                        $seqNo = 1;
+                    }
 
-    //                 // 최대값 보정 (현재 maxSeqNo + 1 보다 크면 맨 뒤)
-    //                 if ($seqNo > $maxSeqNo + 1) {
-    //                     $seqNo = $maxSeqNo + 1;
-    //                 }
+                    // 최대값 보정 (현재 maxSeqNo + 1 보다 크면 맨 뒤)
+                    if ($seqNo > $maxSeqNo + 1) {
+                        $seqNo = $maxSeqNo + 1;
+                    }
 
-    //                 // 이미 해당 seq_no가 존재하면 이후 항목들의 seq_no 1씩 증가
-    //                 if ($$this->scheduleItemRepository->existsSeqNo(
-    //                     $tripDayId,
-    //                     $seqNo
-    //                 )) {
-    //                     $this->scheduleItemRepository->incrementSeqNos(
-    //                         $tripDayId,
-    //                         $seqNo
-    //                     );
-    //                 }
-    //             }
+                    // 이미 해당 seq_no가 존재하면 이후 항목들의 seq_no 1씩 증가
+                    if ($$this->scheduleItemRepository->existsSeqNo(
+                        $tripDayId,
+                        $seqNo
+                    )) {
+                        $this->scheduleItemRepository->incrementSeqNos(
+                            $tripDayId,
+                            $seqNo
+                        );
+                    }
+                }
 
-    //             // ScheduleItem 생성
-    //             return $this->scheduleItemRepository->createScheduleItem(
-    //                 $tripDayId,
-    //                 $seqNo,
+                // ScheduleItem 생성
+                $item = $this->scheduleItemRepository->create([
+                    'trip_day_id' => $tripDayId,
+                    'place_id' => $placeId,
+                    'seq_no' => $seqNo,
+                    'visit_time' => $visitTime,
+                    'memo' => $memo
+                ]);
 
-    // }
+                return $item;
+        });
+
+    }
+
+    /**
+     * 4. ScheduleItem 단건 조회
+     * @param Trip $trip
+     * @param int $dayNo
+     * @param int $seqNo
+     * @return ScheduleItem
+     * @throws ModelNotFoundException
+     */
+    public function getScheduleItem(
+        Trip $trip,
+        int $dayNo,
+        int $seqNo
+    ): ScheduleItem {
+        // tripDayId 조회
+        $tripDayId = $this->getTripDayIdOrFail($trip, $dayNo);
+
+        // ScheduleItem 단건 조회
+        $item = $this->scheduleItemRepository->findByTripDayIdAndSeqNo(
+            $tripDayId,
+            $seqNo
+        );
+
+        if (is_null($item)) {
+            throw new ModelNotFoundException("해당하는 Schedule Item을 찾을 수 없습니다");
+        }
+
+        return $item;
+    }
+
+    /**
+     * 5. ScheduleItem 메모/방문시간 수정
+     * = 둘 중 일부만 수정 가능
+     * @param Trip $trip
+     * @param int $dayNo
+     * @param int $seqNo
+     * @param string|null $visitTime
+     * @param string|null $memo
+     * @return ScheduleItem
+     * @throws ModelNotFoundException
+     */
+    public function updateScheduleItem(
+        Trip $trip,
+        int $dayNo,
+        int $seqNo,
+        ?string $visitTime,
+        ?string $memo
+    ): ScheduleItem {
+        // tripDayId 조회
+        $tripDayId = $this->getTripDayIdOrFail($trip, $dayNo);
+
+        $item = $this->scheduleItemRepository->findByTripDayIdAndSeqNo(
+            $tripDayId,
+            $seqNo
+        );
+
+        // 없으면 예외 발생
+        if (is_null($item)) {
+            throw new ModelNotFoundException("해당하는 Schedule Item을 찾을 수 없습니다");
+        }
+
+        // 메모/방문시간 수정
+        if (!is_null($visitTime)) {
+            $item->visit_time = $visitTime;
+        }
+        if (!is_null($memo)) {
+            $item->memo = $memo;
+        }
+
+        $item->save();
+
+        return $item;
+    }
+
+    /**
+     * 6. ScheduleItem 삭제
+     * - 이후 항목들의 seq_no 1씩 감소
+     * @param Trip $trip
+     * @param int $dayNo
+     * @param int $seqNo
+     * @return void
+     * @throws ModelNotFoundException
+     */
+    public function deleteScheduleItem(
+        Trip $trip,
+        int $dayNo,
+        int $seqNo
+    ): void {
+        // tripDayId 조회
+        $tripDayId = $this->getTripDayIdOrFail($trip, $dayNo);
+
+        DB::transaction(function () use ($tripDayId, $seqNo) {
+
+            // ScheduleItem 삭제 대상 조회
+            $item = $this->scheduleItemRepository->findByTripDayIdAndSeqNo(
+                $tripDayId,
+                $seqNo
+            );
+
+            // 없으면 예외 발생
+            if (is_null($item)) {
+                throw new ModelNotFoundException("해당하는 Schedule Item을 찾을 수 없습니다");
+            }
+
+            // ScheduleItem 삭제
+            $item->delete();
+
+            // 이후 항목들의 seq_no 1씩 감소
+            $this->scheduleItemRepository->decrementSeqNos(
+                $tripDayId,
+                $seqNo
+            );
+        });
+    }
+
+    /**
+     * 7. ScheduleItem 재배치
+     * - 같은 TripDay 내에서만 이동 가능
+     * @param Trip $trip
+     * @param int $dayNo
+     * @param int $oldSeqNo // 기존 순번
+     * @param int $newSeqNo // 새로운 순번
+     * @return void
+     * @throws ModelNotFoundException
+     */
+    public function reorderScheduleItem(
+        Trip $trip,
+        int $dayNo,
+        int $oldSeqNo,
+        int $newSeqNo
+    ): void {
+        // tripDayId 조회
+        $tripDayId = $this->getTripDayIdOrFail($trip, $dayNo);
+
+        DB::transaction(function () use (
+            $tripDayId,
+            $oldSeqNo,
+            $newSeqNo
+        ) {
+            // ScheduleItem 이동 대상 조회
+            $item = $this->scheduleItemRepository->findByTripDayIdAndSeqNo(
+                $tripDayId,
+                $oldSeqNo
+            );
+
+            // 없으면 예외 발생
+            if (is_null($item)) {
+                throw new ModelNotFoundException("해당하는 Schedule Item을 찾을 수 없습니다");
+            }
+
+            // newSeqNo 보정
+            $maxSeqNo = $this->scheduleItemRepository->getMaxSeqNo($tripDayId);
+            
+            if ($maxSeqNo === 0) {
+                throw new ModelNotFoundException("재배치할 Schedule Item이 없습니다");
+            }
+
+            if ($newSeqNo < 1) {
+                $newSeqNo = 1;
+            }
+            else if ($newSeqNo > $maxSeqNo) {
+                $newSeqNo = $maxSeqNo;
+            }
+
+            if ($oldSeqNo === $newSeqNo) {
+                // 이동 없음
+                return;
+            }
+
+            if ($oldSeqNo < $newSeqNo) {
+                // 앞으로 이동: oldSeqNo+1 ~ newSeqNo 항목들 seq_no 1씩 감소
+                $this->scheduleItemRepository->decrementSeqNos(
+                    $tripDayId,
+                    $oldSeqNo
+                );
+            } else {
+                // 뒤로 이동: newSeqNo ~ oldSeqNo-1 항목들 seq_no 1씩 증가
+                $this->scheduleItemRepository->incrementSeqNos(
+                    $tripDayId,
+                    $newSeqNo
+                );
+            }
+
+            // 대상 항목의 seq_no 업데이트
+            $this->scheduleItemRepository->updateSeqNo(
+                $tripDayId,
+                $oldSeqNo,
+                $newSeqNo
+            );
+        });
+    }
+
 }
