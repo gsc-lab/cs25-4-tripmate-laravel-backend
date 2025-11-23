80f187f04b4eb2df2156477b491656fd4e3954b4
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-24 03:13:53 +0900

    region 리포지토리 작성

--- backend/app/Repositories/Region/RegionRepository.php
+++ backend/app/Repositories/Region/RegionRepository.php
@@ -1,3 +1,33 @@
 <?php
+    namespace App\Repositories\Region;
 
-// RegionRepository: Region 쿼리 전담
+    use App\Repositories\BaseRepository;
+    use App\Models\Region;
+
+    class RegionRepository extends BaseRepository
+    {
+        public function __construct(Region $region)
+        {
+            parent::__construct($region);
+        }
+
+        /**
+         * find Regions
+         * @param mixed $query
+         * @return \Illuminate\Database\Eloquent\Collection<int, TModel>
+         */
+        public function findRegions($query)
+        {
+            return $this->model->where('name', 'like', '%'. $query .'%')->get();
+        }
+
+        /**
+         * selected Regions
+         * @param mixed $country
+         * @return \Illuminate\Database\Eloquent\Collection<int, TModel>
+         */
+        public function selectRegions($country)
+        {
+            return $this->model->where("country_code", $country)->get();
+        }
+    }
