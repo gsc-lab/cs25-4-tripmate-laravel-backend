77a9e7d5d6c112f64e4f57113a4454aa22b28d11
jammmin02 <jmkm1201@naver.com>
2025-11-24 13:00:59 +0900

    fix : namespace 경로 수정

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\User\UsersController;
+use App\Http\Controllers\Auth\UsersController;
 use App\Http\Controllers\Trip\TripController;
-use App\Http\Controllers\TripDay\TripDayController;
-use App\Http\Controllers\ScheduleItem\ScheduleItemController;
+use App\Http\Controllers\Trip\TripDayController;
+use App\Http\Controllers\Trip\ScheduleItemController;
 use App\Http\Controllers\Place\PlaceController;
 use App\Http\Controllers\Region\RegionController;  
 
