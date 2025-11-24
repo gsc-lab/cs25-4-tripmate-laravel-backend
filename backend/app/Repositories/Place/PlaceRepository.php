459ebd0ca385fa3bb0b00c4634a4b3d1abaedaf4
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-25 05:53:49 +0900

    placeRepository 작성

--- backend/app/Repositories/Place/PlaceRepository.php
+++ backend/app/Repositories/Place/PlaceRepository.php
@@ -1,4 +1,37 @@
 <?php
+    namespace App\Repositories\Place;
 
-// PlaceRepository: Place 쿼리 전담
+    use App\Repositories\BaseRepository;
+    use App\Models\Place;
+    use App\Models\PlaceCategory;
 
+    class PlaceRepository extends BaseRepository
+    {
+        public function __construct(Place $place)
+        {
+            parent::__construct($place);
+        }
+
+        public function update(array $data)
+        {
+            $categoryCode = $data['category'] ?? 'etc';
+
+            // 카테고리로 해당 id 찾아 반환 (없을 경우 etc id 반환)
+            $category = PlaceCategory::where('code', $categoryCode)->first();
+            $categoryId = $category ? $category->category_id : 288;
+
+            // 값 Upsert
+            $place = $this->model->updateOrCreate(
+                ['external_ref'=> $data['external_ref']],
+                [
+                    'name'         => $data['name'],
+                    'address'      => $data['address'],
+                    'lat'          => $data['lat'],
+                    'lng'          => $data['lng'],
+                    'category_id'  => $categoryId
+                ]
+            );
+
+            return $place;
+        }
+    }
