73a0bf603bbbada5f75e38222a39edf390df9e38
jammmin02 <jmkm1201@naver.com>
2025-11-24 13:17:32 +0900

    fix :  dayno 관련 메서드 수정

--- backend/app/Repositories/Trip/TripDayRepository.php
+++ backend/app/Repositories/Trip/TripDayRepository.php
@@ -90,11 +90,20 @@ class TripDayRepository extends BaseRepository
    */
   public function incrementDayNo(int $tripId, int $fromDayNo): int
   {
-    return $this->model
+    // 해당 일차 이후의 row 들 조회 
+    $rows = $this->model
       ->newQuery()
       ->where('trip_id', $tripId)
       ->where('day_no', '>=', $fromDayNo)
-      ->increment('day_no');
+      ->orderByDesc('day_no')
+      ->get();
+
+    // 각 row 들의 day_no 증가
+    foreach ($rows as $row) {
+      $row->increment('day_no');
+    }
+
+    return $rows->count();
   }
 
   /**
@@ -106,11 +115,20 @@ class TripDayRepository extends BaseRepository
    */
   public function decrementDayNoAfter(int $tripId, int $fromDayNo): int
   {
-    return $this->model
+    // 해당 일차 이후의 row 들 조회
+    $rows = $this->model
       ->newQuery()
       ->where('trip_id', $tripId)
       ->where('day_no', '>', $fromDayNo)
-      ->decrement('day_no');
+      ->orderBy('day_no', 'asc')
+      ->get();
+
+    // 각 row 들의 day_no 감소
+    foreach ($rows as $row) {
+      $row->decrement('day_no');
+    }
+
+    return $rows->count();
   }
 
   /**
