460745988b5e81271c96d52b1413bb5465fbeb1c
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-30 02:50:18 +0900

    url & fieldmask 추가

--- backend/config/services.php
+++ backend/config/services.php
@@ -37,6 +37,24 @@ return [
 
     'googleApi'=> [
         'api_key' => env('GOOGLE_API_KEY'),
+    ],
+
+    'google_places' => [
+
+        // API 엔드포인트
+        'endpoints' => [
+            'autocomplete'     => 'https://places.googleapis.com/v1/places:autocomplete',
+            'text_search'      => 'https://places.googleapis.com/v1/places:searchText',
+            'reverse_geocoding'=> 'https://maps.googleapis.com/maps/api/geocode/json',
+            'nearby'           => 'https://places.googleapis.com/v1/places:searchNearby',
+        ],
+
+        // FieldMask
+        'field_masks' => [
+            'search'         => 'places.id,places.displayName,places.formattedAddress,places.location,places.primaryType,nextPageToken',
+            'nearby'         => 'places.id,places.displayName,places.formattedAddress,places.location,places.primaryType',
+            'place_details'  => 'id,displayName,formattedAddress,location,primaryType',
+        ]
     ]
 
 ];
